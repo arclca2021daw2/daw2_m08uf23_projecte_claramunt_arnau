11ae15ce4ac993a74d5c0d3dd18e1c3da6c02c72
arclca <arclca2021daw2@gmail.com>
2021-03-09 20:28:00 +0100

    Afegir usuari

---
+++ afegir.php
@@ -0,0 +1,59 @@
+<?php
+require 'vendor/autoload.php';
+use Laminas\Ldap\Attribute;
+use Laminas\Ldap\Ldap;
+
+ini_set('display_errors', 0);
+
+$uid = $_POST["uid"];
+$unorg= $_POST["unorg"];
+$num_id= $_POST["num_id"];
+$grup= $_POST["grup"];
+$dir_pers= $_POST["dir_pers"];
+$sh= $_POST["sh"];
+$cn= $_POST["cn"];
+$sn= $_POST["sn"];
+$nom= $_POST["nom"];
+$mobil= $_POST["mobil"];
+$adressa= $_POST["adressa"];
+$telefon= $_POST["telefon"];
+$titol= $_POST["titol"];
+$descripcio= $_POST["descripcio"];
+$objcl=array('inetOrgPerson','organizationalPerson','person','posixAccount','shadowAccount','top');
+
+$domini = 'dc=fjeclot,dc=net';
+$opcions = [
+    'host' => 'zend-arclca.fjeclot.net',
+    'username' => "cn=admin,$domini",
+    'password' => 'fjeclot',
+    'bindRequiresDn' => true,
+    'accountDomainName' => 'fjeclot.net',
+    'baseDn' => 'dc=fjeclot,dc=net',
+];
+$ldap = new Ldap($opcions);
+$ldap->bind();
+$nova_entrada = [];
+Attribute::setAttribute($nova_entrada, 'objectClass', $objcl);
+Attribute::setAttribute($nova_entrada, 'uid', $uid);
+Attribute::setAttribute($nova_entrada, 'uidNumber', $num_id);
+Attribute::setAttribute($nova_entrada, 'gidNumber', $grup);
+Attribute::setAttribute($nova_entrada, 'homeDirectory', $dir_pers);
+Attribute::setAttribute($nova_entrada, 'loginShell', $sh);
+Attribute::setAttribute($nova_entrada, 'cn', $cn);
+Attribute::setAttribute($nova_entrada, 'sn', $sn);
+Attribute::setAttribute($nova_entrada, 'givenName', $nom);
+Attribute::setAttribute($nova_entrada, 'mobile', $mobil);
+Attribute::setAttribute($nova_entrada, 'postalAddress', $adressa);
+Attribute::setAttribute($nova_entrada, 'telephoneNumber', $telefon);
+Attribute::setAttribute($nova_entrada, 'title', $titol);
+Attribute::setAttribute($nova_entrada, 'description', $descripcio);
+$dn = 'uid='.$uid.',ou='.$unorg.',dc=fjeclot,dc=net';
+if($ldap->add($dn, $nova_entrada)) {
+    echo "Usuari creat";
+} else {
+    echo "No s'ha pogut crear l'usuari";
+}
+?>
+
+<br>
+<a href="http://zend-arclca.fjeclot.net/projecte/menu.php">Tornar</a>
--- menu.php
+++ menu.php
@@ -3,14 +3,39 @@
 		<title>
 			Menú Projecte LDAP
 		</title>
+		<link rel="stylesheet" href="./menu.css">
 	</head>
 	<body>
 		<h1>MENÚ PRINCIPAL</h1>
 		<h2>Visualitzar un usuari</h2>
 		<form action="http://zend-arclca.fjeclot.net/projecte/mostrar.php" method="GET">
-			Identificador: <input type="text" name="usr"><br>
-			Unitat organitzativa: <input type="text" name="ou"><br>
+			Identificador: <input type="text" name="usr" required><br>
+			Unitat organitzativa: <input type="text" name="ou" required><br>
 			<input type="submit" value="Buscar">
+			<input type="reset" value="Esborrar">
+		</form>
+		
+		<h2>Afegir un usuari</h2>
+		<form action="http://zend-arclca.fjeclot.net/projecte/afegir.php" method="POST">
+    		<div class="divform">
+    			UID: <input type="text" name="uid" required><br>
+    			Unitat organitzativa: <input type="text" name="unorg" required><br>
+    			Número Id: <input type="number" name="num_id" required><br>
+    			Id Grup: <input type="number" name="grup" required><br>
+    			Directori personal: <input type="text" name="dir_pers" required><br>
+    			Shell: <input type="text" name="sh" required><br>
+    			CN: <input type="text" name="cn" required><br>
+    		</div>
+    		<div class="divform">
+    			SN: <input type="text" name="sn" required><br>
+    			Nom: <input type="text" name="nom" required><br>
+    			Adreça Postal: <input type="text" name="adressa" required><br>
+    			Mòbil: <input type="text" name="mobil" required><br>
+    			Telèfon: <input type="text" name="telefon" required><br>
+    			Títol: <input type="text" name="titol" required><br>
+    			Descripció: <input type="text" name="descripcio" required><br>
+    		</div>
+			<input type="submit" value="Afegir">
 			<input type="reset" value="Esborrar">
 		</form>
 		
